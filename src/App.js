838fdf25d28cb17fbddbe00ef66dd7c4291e0162
Jamie Wright <jamie@brilliantfantastic.com>
2019-09-04 21:12:30 -0500

    Move App.js to src

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
 
-import HomeScreen from "./src/screens/HomeScreen";
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import HomeScreen from "./screens/HomeScreen";
 
 const Navigator = createSwitchNavigator(
   {
